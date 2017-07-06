bryanodonoghue/octopress

commit b1444ee1430b1e8d04e15ec23fb3f8cb27907e4d

public: blog: screaming fast gpio with intel galileo
First post - detailing the different methods of doing GPIO on a Galileo
using the Arduino user-space libraries.

Signed-off-by: Bryan O'Donoghue <pure.logic@nexus-software.ie>

public/blog/2017/06/05/screaming-fast-gpio-with-intel-galileo/index.html
@@ -1,24 +1,91 @@
-<?xml version="1.0" encoding="utf-8"?>
-<feed xmlns="http://www.w3.org/2005/Atom">
 
-  <title><![CDATA[A weblog by Bryan]]></title>
-  <link href="http://www.nexus-software.ie/atom.xml" rel="self"/>
-  <link href="http://www.nexus-software.ie/"/>
-  <updated>2017-06-05T14:38:22+01:00</updated>
-  <id>http://www.nexus-software.ie/</id>
-  <author>
-    <name><![CDATA[Bryan O'Donoghue]]></name>
-    
-  </author>
-  <generator uri="http://octopress.org/">Octopress</generator>
+<!DOCTYPE html>
+<!--[if IEMobile 7 ]><html class="no-js iem7"><![endif]-->
+<!--[if lt IE 9]><html class="no-js lte-ie8"><![endif]-->
+<!--[if (gt IE 8)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html class="no-js" lang="en"><!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+  <title>Screaming Fast GPIO With Intel Galileo - A weblog by Bryan</title>
+  <meta name="author" content="Bryan O'Donoghue">
 
   
-  <entry>
-    <title type="html"><![CDATA[Screaming Fast GPIO With Intel Galileo]]></title>
-    <link href="http://www.nexus-software.ie/blog/2017/06/05/screaming-fast-gpio-with-intel-galileo/"/>
-    <updated>2017-06-05T09:37:45+01:00</updated>
-    <id>http://www.nexus-software.ie/blog/2017/06/05/screaming-fast-gpio-with-intel-galileo</id>
-    <content type="html"><![CDATA[<p><img src="images/galileo.jpg"></p>
+  <meta name="description" content="Foreword Intel Galileo and the Quark X1000 SoC that power the Galileo, are projects on
+which I was the leading Linux engineer. One problem we had &hellip;">
+  
+
+  <!-- http://t.co/dKP3o1e -->
+  <meta name="HandheldFriendly" content="True">
+  <meta name="MobileOptimized" content="320">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  
+  <link rel="canonical" href="http://www.nexus-software.ie/blog/2017/06/05/screaming-fast-gpio-with-intel-galileo/">
+  <link href="/octopress/public/favicon.png" rel="icon">
+  <link href="/octopress/public/stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css">
+  <link href="/atom.xml" rel="alternate" title="A weblog by Bryan" type="application/atom+xml">
+  <script src="/octopress/public/javascripts/modernizr-2.0.js"></script>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+  <script>!window.jQuery && document.write(unescape('%3Cscript src="/javascripts/libs/jquery.min.js"%3E%3C/script%3E'))</script>
+  <script src="/octopress/public/javascripts/octopress.js" type="text/javascript"></script>
+  <!--Fonts from Google"s Web font directory at http://google.com/webfonts -->
+<link href="//fonts.googleapis.com/css?family=PT+Serif:regular,italic,bold,bolditalic" rel="stylesheet" type="text/css">
+<link href="//fonts.googleapis.com/css?family=PT+Sans:regular,italic,bold,bolditalic" rel="stylesheet" type="text/css">
+
+  
+
+</head>
+
+<body   >
+  <header role="banner"><hgroup>
+  <h1><a href="/octopress/public/">A weblog by Bryan</a></h1>
+  
+    <h2>A collection of thoughts, articles and stuff by Bryan</h2>
+  
+</hgroup>
+
+</header>
+  <nav role="navigation"><ul class="subscription" data-subscription="rss">
+  <li><a href="/atom.xml" rel="subscribe-rss" title="subscribe via RSS">RSS</a></li>
+  
+</ul>
+  
+<form action="https://www.google.com/search" method="get">
+  <fieldset role="search">
+    <input type="hidden" name="sitesearch" value="www.nexus-software.ie">
+    <input class="search" type="text" name="q" results="0" placeholder="Search"/>
+  </fieldset>
+</form>
+  
+<ul class="main-navigation">
+  <li><a href="/octopress/public/">Blog</a></li>
+  <li><a href="/octopress/public/blog/archives">Archives</a></li>
+</ul>
+
+</nav>
+  <div id="main">
+    <div id="content">
+      <div>
+<article class="hentry" role="article">
+  
+  <header>
+    
+      <h1 class="entry-title">Screaming Fast GPIO With Intel Galileo</h1>
+    
+    
+      <p class="meta">
+        
+
+
+
+
+<time class='entry-date' datetime='2017-06-05T09:37:45+01:00'><span class='date'><span class='date-month'>Jun</span> <span class='date-day'>5</span><span class='date-suffix'>th</span>, <span class='date-year'>2017</span></span> <span class='time'>9:37 am</span></time>
+        
+      </p>
+    
+  </header>
+
+
+<div class="entry-content"><p><img src="images/galileo.jpg"></p>
 
 <h1>Foreword</h1>
 
@@ -276,7 +343,151 @@ times faster for a given wave form - hence ~700kHz (680kHz) becomes ~2.8Mhz
 <p>In other words the responsibility lies with the application designer in cases 3
 and 4 to ensure the GPIO register values are correct - assuming - these values
 matter to the application use-case.</p>
-]]></content>
-  </entry>
+</div>
+
+
+  <footer>
+    <p class="meta">
+      
   
-</feed>
+
+<span class="byline author vcard">Posted by <span class="fn">Bryan O'Donoghue</span></span>
+
+      
+
+
+
+
+<time class='entry-date' datetime='2017-06-05T09:37:45+01:00'><span class='date'><span class='date-month'>Jun</span> <span class='date-day'>5</span><span class='date-suffix'>th</span>, <span class='date-year'>2017</span></span> <span class='time'>9:37 am</span></time>
+      
+
+<span class="categories">
+  
+    <a class='category' href='/octopress/public/blog/categories/arduino/'>arduino</a>, <a class='category' href='/octopress/public/blog/categories/galileo/'>galileo</a>, <a class='category' href='/octopress/public/blog/categories/intel/'>intel</a>, <a class='category' href='/octopress/public/blog/categories/linux/'>linux</a>, <a class='category' href='/octopress/public/blog/categories/x86/'>x86</a>
+  
+</span>
+
+
+    </p>
+    
+      <div class="sharing">
+  
+  <a href="//twitter.com/share" class="twitter-share-button" data-url="http://www.nexus-software.ie/blog/2017/06/05/screaming-fast-gpio-with-intel-galileo/" data-via="" data-counturl="http://www.nexus-software.ie/blog/2017/06/05/screaming-fast-gpio-with-intel-galileo/" >Tweet</a>
+  
+  
+  <div class="g-plusone" data-size="medium"></div>
+  
+  
+    <div class="fb-like" data-send="true" data-width="450" data-show-faces="false"></div>
+  
+</div>
+
+    
+    <p class="meta">
+      
+      
+    </p>
+  </footer>
+</article>
+
+</div>
+
+<aside class="sidebar">
+  
+    <section>
+  <h1>Recent Posts</h1>
+  <ul id="recent_posts">
+    
+      <li class="post">
+        <a href="/octopress/public/blog/2017/06/05/screaming-fast-gpio-with-intel-galileo/">Screaming Fast GPIO With Intel Galileo</a>
+      </li>
+    
+  </ul>
+</section>
+
+<section>
+  <h1>GitHub Repos</h1>
+  <ul id="gh_repos">
+    <li class="loading">Status updating...</li>
+  </ul>
+  
+  <a href="https://github.com/bryanodonoghue">@bryanodonoghue</a> on GitHub
+  
+  <script type="text/javascript">
+    $(document).ready(function(){
+        if (!window.jXHR){
+            var jxhr = document.createElement('script');
+            jxhr.type = 'text/javascript';
+            jxhr.src = '/javascripts/libs/jXHR.js';
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(jxhr, s);
+        }
+
+        github.showRepos({
+            user: 'bryanodonoghue',
+            count: 0,
+            skip_forks: true,
+            target: '#gh_repos'
+        });
+    });
+  </script>
+  <script src="/octopress/public/javascripts/github.js" type="text/javascript"> </script>
+</section>
+
+
+
+
+
+  
+</aside>
+
+
+    </div>
+  </div>
+  <footer role="contentinfo"><p>
+  Copyright &copy; 2017 - Bryan O'Donoghue -
+  <span class="credit">Powered by <a href="http://octopress.org">Octopress</a></span>
+</p>
+
+</footer>
+  
+
+
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {return;}
+  js = d.createElement(s); js.id = id; js.async = true;
+  js.src = "//connect.facebook.net/en_US/all.js#appId=212934732101925&xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+
+
+  <script type="text/javascript">
+    (function() {
+      var script = document.createElement('script'); script.type = 'text/javascript'; script.async = true;
+      script.src = 'https://apis.google.com/js/plusone.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(script, s);
+    })();
+  </script>
+
+
+
+  <script type="text/javascript">
+    (function(){
+      var twitterWidgets = document.createElement('script');
+      twitterWidgets.type = 'text/javascript';
+      twitterWidgets.async = true;
+      twitterWidgets.src = '//platform.twitter.com/widgets.js';
+      document.getElementsByTagName('head')[0].appendChild(twitterWidgets);
+    })();
+  </script>
+
+
+
+
+
+</body>
+</html>

public/blog/categories/arduino/atom.xml
@@ -4,7 +4,7 @@
   <title><![CDATA[Category: Arduino | A weblog by Bryan]]></title>
   <link href="http://www.nexus-software.ie/blog/categories/arduino/atom.xml" rel="self"/>
   <link href="http://www.nexus-software.ie/"/>
-  <updated>2017-06-05T14:20:37+01:00</updated>
+  <updated>2017-06-05T14:38:22+01:00</updated>
   <id>http://www.nexus-software.ie/</id>
   <author>
     <name><![CDATA[Bryan O'Donoghue]]></name>

public/blog/categories/galileo/atom.xml
@@ -4,7 +4,7 @@
   <title><![CDATA[Category: Galileo | A weblog by Bryan]]></title>
   <link href="http://www.nexus-software.ie/blog/categories/galileo/atom.xml" rel="self"/>
   <link href="http://www.nexus-software.ie/"/>
-  <updated>2017-06-05T14:20:37+01:00</updated>
+  <updated>2017-06-05T14:38:22+01:00</updated>
   <id>http://www.nexus-software.ie/</id>
   <author>
     <name><![CDATA[Bryan O'Donoghue]]></name>

public/blog/categories/intel/atom.xml
@@ -4,7 +4,7 @@
   <title><![CDATA[Category: Intel | A weblog by Bryan]]></title>
   <link href="http://www.nexus-software.ie/blog/categories/intel/atom.xml" rel="self"/>
   <link href="http://www.nexus-software.ie/"/>
-  <updated>2017-06-05T14:20:37+01:00</updated>
+  <updated>2017-06-05T14:38:22+01:00</updated>
   <id>http://www.nexus-software.ie/</id>
   <author>
     <name><![CDATA[Bryan O'Donoghue]]></name>

public/blog/categories/linux/atom.xml
@@ -4,7 +4,7 @@
   <title><![CDATA[Category: Linux | A weblog by Bryan]]></title>
   <link href="http://www.nexus-software.ie/blog/categories/linux/atom.xml" rel="self"/>
   <link href="http://www.nexus-software.ie/"/>
-  <updated>2017-06-05T14:20:37+01:00</updated>
+  <updated>2017-06-05T14:38:22+01:00</updated>
   <id>http://www.nexus-software.ie/</id>
   <author>
     <name><![CDATA[Bryan O'Donoghue]]></name>

public/blog/categories/x86/atom.xml
@@ -4,7 +4,7 @@
   <title><![CDATA[Category: X86 | A weblog by Bryan]]></title>
   <link href="http://www.nexus-software.ie/blog/categories/x86/atom.xml" rel="self"/>
   <link href="http://www.nexus-software.ie/"/>
-  <updated>2017-06-05T14:20:37+01:00</updated>
+  <updated>2017-06-05T14:38:22+01:00</updated>
   <id>http://www.nexus-software.ie/</id>
   <author>
     <name><![CDATA[Bryan O'Donoghue]]></name>

public/index.html
@@ -1,24 +1,94 @@
-<?xml version="1.0" encoding="utf-8"?>
-<feed xmlns="http://www.w3.org/2005/Atom">
 
-  <title><![CDATA[A weblog by Bryan]]></title>
-  <link href="http://www.nexus-software.ie/atom.xml" rel="self"/>
-  <link href="http://www.nexus-software.ie/"/>
-  <updated>2017-06-05T14:38:22+01:00</updated>
-  <id>http://www.nexus-software.ie/</id>
-  <author>
-    <name><![CDATA[Bryan O'Donoghue]]></name>
-    
-  </author>
-  <generator uri="http://octopress.org/">Octopress</generator>
+<!DOCTYPE html>
+<!--[if IEMobile 7 ]><html class="no-js iem7"><![endif]-->
+<!--[if lt IE 9]><html class="no-js lte-ie8"><![endif]-->
+<!--[if (gt IE 8)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html class="no-js" lang="en"><!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+  <title>A weblog by Bryan</title>
+  <meta name="author" content="Bryan O'Donoghue">
 
   
-  <entry>
-    <title type="html"><![CDATA[Screaming Fast GPIO With Intel Galileo]]></title>
-    <link href="http://www.nexus-software.ie/blog/2017/06/05/screaming-fast-gpio-with-intel-galileo/"/>
-    <updated>2017-06-05T09:37:45+01:00</updated>
-    <id>http://www.nexus-software.ie/blog/2017/06/05/screaming-fast-gpio-with-intel-galileo</id>
-    <content type="html"><![CDATA[<p><img src="images/galileo.jpg"></p>
+  <meta name="description" content="Foreword Intel Galileo and the Quark X1000 SoC that power the Galileo, are projects on
+which I was the leading Linux engineer. One problem we had &hellip;">
+  
+
+  <!-- http://t.co/dKP3o1e -->
+  <meta name="HandheldFriendly" content="True">
+  <meta name="MobileOptimized" content="320">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  
+  <link rel="canonical" href="http://www.nexus-software.ie/">
+  <link href="/octopress/public/favicon.png" rel="icon">
+  <link href="/octopress/public/stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css">
+  <link href="/atom.xml" rel="alternate" title="A weblog by Bryan" type="application/atom+xml">
+  <script src="/octopress/public/javascripts/modernizr-2.0.js"></script>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+  <script>!window.jQuery && document.write(unescape('%3Cscript src="/javascripts/libs/jquery.min.js"%3E%3C/script%3E'))</script>
+  <script src="/octopress/public/javascripts/octopress.js" type="text/javascript"></script>
+  <!--Fonts from Google"s Web font directory at http://google.com/webfonts -->
+<link href="//fonts.googleapis.com/css?family=PT+Serif:regular,italic,bold,bolditalic" rel="stylesheet" type="text/css">
+<link href="//fonts.googleapis.com/css?family=PT+Sans:regular,italic,bold,bolditalic" rel="stylesheet" type="text/css">
+
+  
+
+</head>
+
+<body   >
+  <header role="banner"><hgroup>
+  <h1><a href="/octopress/public/">A weblog by Bryan</a></h1>
+  
+    <h2>A collection of thoughts, articles and stuff by Bryan</h2>
+  
+</hgroup>
+
+</header>
+  <nav role="navigation"><ul class="subscription" data-subscription="rss">
+  <li><a href="/atom.xml" rel="subscribe-rss" title="subscribe via RSS">RSS</a></li>
+  
+</ul>
+  
+<form action="https://www.google.com/search" method="get">
+  <fieldset role="search">
+    <input type="hidden" name="sitesearch" value="www.nexus-software.ie">
+    <input class="search" type="text" name="q" results="0" placeholder="Search"/>
+  </fieldset>
+</form>
+  
+<ul class="main-navigation">
+  <li><a href="/octopress/public/">Blog</a></li>
+  <li><a href="/octopress/public/blog/archives">Archives</a></li>
+</ul>
+
+</nav>
+  <div id="main">
+    <div id="content">
+      <div class="blog-index">
+  
+  
+  
+    <article>
+      
+  <header>
+    
+      <h1 class="entry-title"><a href="/octopress/public/blog/2017/06/05/screaming-fast-gpio-with-intel-galileo/">Screaming Fast GPIO With Intel Galileo</a></h1>
+    
+    
+      <p class="meta">
+        
+
+
+
+
+<time class='entry-date' datetime='2017-06-05T09:37:45+01:00'><span class='date'><span class='date-month'>Jun</span> <span class='date-day'>5</span><span class='date-suffix'>th</span>, <span class='date-year'>2017</span></span> <span class='time'>9:37 am</span></time>
+        
+      </p>
+    
+  </header>
+
+
+  <div class="entry-content"><p><img src="images/galileo.jpg"></p>
 
 <h1>Foreword</h1>
 
@@ -276,7 +346,114 @@ times faster for a given wave form - hence ~700kHz (680kHz) becomes ~2.8Mhz
 <p>In other words the responsibility lies with the application designer in cases 3
 and 4 to ensure the GPIO register values are correct - assuming - these values
 matter to the application use-case.</p>
-]]></content>
-  </entry>
+</div>
   
-</feed>
+  
+
+
+    </article>
+  
+  <div class="pagination">
+    
+    <a href="/octopress/public/blog/archives">Blog Archives</a>
+    
+  </div>
+</div>
+<aside class="sidebar">
+  
+    <section>
+  <h1>Recent Posts</h1>
+  <ul id="recent_posts">
+    
+      <li class="post">
+        <a href="/octopress/public/blog/2017/06/05/screaming-fast-gpio-with-intel-galileo/">Screaming Fast GPIO With Intel Galileo</a>
+      </li>
+    
+  </ul>
+</section>
+
+<section>
+  <h1>GitHub Repos</h1>
+  <ul id="gh_repos">
+    <li class="loading">Status updating...</li>
+  </ul>
+  
+  <a href="https://github.com/bryanodonoghue">@bryanodonoghue</a> on GitHub
+  
+  <script type="text/javascript">
+    $(document).ready(function(){
+        if (!window.jXHR){
+            var jxhr = document.createElement('script');
+            jxhr.type = 'text/javascript';
+            jxhr.src = '/javascripts/libs/jXHR.js';
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(jxhr, s);
+        }
+
+        github.showRepos({
+            user: 'bryanodonoghue',
+            count: 0,
+            skip_forks: true,
+            target: '#gh_repos'
+        });
+    });
+  </script>
+  <script src="/octopress/public/javascripts/github.js" type="text/javascript"> </script>
+</section>
+
+
+
+
+
+  
+</aside>
+
+    </div>
+  </div>
+  <footer role="contentinfo"><p>
+  Copyright &copy; 2017 - Bryan O'Donoghue -
+  <span class="credit">Powered by <a href="http://octopress.org">Octopress</a></span>
+</p>
+
+</footer>
+  
+
+
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {return;}
+  js = d.createElement(s); js.id = id; js.async = true;
+  js.src = "//connect.facebook.net/en_US/all.js#appId=212934732101925&xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+
+
+  <script type="text/javascript">
+    (function() {
+      var script = document.createElement('script'); script.type = 'text/javascript'; script.async = true;
+      script.src = 'https://apis.google.com/js/plusone.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(script, s);
+    })();
+  </script>
+
+
+
+  <script type="text/javascript">
+    (function(){
+      var twitterWidgets = document.createElement('script');
+      twitterWidgets.type = 'text/javascript';
+      twitterWidgets.async = true;
+      twitterWidgets.src = '//platform.twitter.com/widgets.js';
+      document.getElementsByTagName('head')[0].appendChild(twitterWidgets);
+    })();
+  </script>
+
+
+
+
+
+</body>
+</html>

commit 3110b13637841c38d817cd97b18d4f0110ac038c

public: blog: fix path to galileo jpg
Fixup the path to the galileo jpg. It turns out in octopress you need to
prefix {% img images/someimage.jpg %} with / => {% /images/someimage.jpg %}
otherwise you get a broken link when the blog entry isn't displayed on the
front-page.

https://ablwr.github.io/blog/2014/09/25/why-wont-octopress-just-upload-my-images/

fixes: commit b1444ee1430b ('public: blog: screaming fast gpio with intel galileo')

Signed-off-by: Bryan O'Donoghue <pure.logic@nexus-software.ie>

public/atom.xml
@@ -4,7 +4,7 @@
   <title><![CDATA[A weblog by Bryan]]></title>
   <link href="http://www.nexus-software.ie/atom.xml" rel="self"/>
   <link href="http://www.nexus-software.ie/"/>
-  <updated>2017-06-07T20:33:48+01:00</updated>
+  <updated>2017-07-06T10:19:07+01:00</updated>
   <id>http://www.nexus-software.ie/</id>
   <author>
     <name><![CDATA[Bryan O'Donoghue]]></name>
@@ -18,7 +18,7 @@
     <link href="http://www.nexus-software.ie/blog/2017/06/05/screaming-fast-gpio-with-intel-galileo/"/>
     <updated>2017-06-05T09:37:45+01:00</updated>
     <id>http://www.nexus-software.ie/blog/2017/06/05/screaming-fast-gpio-with-intel-galileo</id>
-    <content type="html"><![CDATA[<p><img src="images/galileo.jpg"></p>
+    <content type="html"><![CDATA[<p><img src="http://www.nexus-software.ie/images/galileo.jpg"></p>
 
 <h1>Foreword</h1>
 

public/blog/2017/06/05/screaming-fast-gpio-with-intel-galileo/index.html
@@ -85,7 +85,7 @@ which I was the leading Linux engineer. One problem we had &hellip;">
   </header>
 
 
-<div class="entry-content"><p><img src="images/galileo.jpg"></p>
+<div class="entry-content"><p><img src="/octopress/public/images/galileo.jpg"></p>
 
 <h1>Foreword</h1>
 

public/blog/categories/arduino/atom.xml
@@ -4,7 +4,7 @@
   <title><![CDATA[Category: Arduino | A weblog by Bryan]]></title>
   <link href="http://www.nexus-software.ie/blog/categories/arduino/atom.xml" rel="self"/>
   <link href="http://www.nexus-software.ie/"/>
-  <updated>2017-06-07T20:33:48+01:00</updated>
+  <updated>2017-07-06T10:19:07+01:00</updated>
   <id>http://www.nexus-software.ie/</id>
   <author>
     <name><![CDATA[Bryan O'Donoghue]]></name>
@@ -18,7 +18,7 @@
     <link href="http://www.nexus-software.ie/blog/2017/06/05/screaming-fast-gpio-with-intel-galileo/"/>
     <updated>2017-06-05T09:37:45+01:00</updated>
     <id>http://www.nexus-software.ie/blog/2017/06/05/screaming-fast-gpio-with-intel-galileo</id>
-    <content type="html"><![CDATA[<p><img src="images/galileo.jpg"></p>
+    <content type="html"><![CDATA[<p><img src="/images/galileo.jpg"></p>
 
 <h1>Foreword</h1>
 

public/blog/categories/galileo/atom.xml
@@ -4,7 +4,7 @@
   <title><![CDATA[Category: Galileo | A weblog by Bryan]]></title>
   <link href="http://www.nexus-software.ie/blog/categories/galileo/atom.xml" rel="self"/>
   <link href="http://www.nexus-software.ie/"/>
-  <updated>2017-06-07T20:33:48+01:00</updated>
+  <updated>2017-07-06T10:19:07+01:00</updated>
   <id>http://www.nexus-software.ie/</id>
   <author>
     <name><![CDATA[Bryan O'Donoghue]]></name>
@@ -18,7 +18,7 @@
     <link href="http://www.nexus-software.ie/blog/2017/06/05/screaming-fast-gpio-with-intel-galileo/"/>
     <updated>2017-06-05T09:37:45+01:00</updated>
     <id>http://www.nexus-software.ie/blog/2017/06/05/screaming-fast-gpio-with-intel-galileo</id>
-    <content type="html"><![CDATA[<p><img src="images/galileo.jpg"></p>
+    <content type="html"><![CDATA[<p><img src="/images/galileo.jpg"></p>
 
 <h1>Foreword</h1>
 

public/blog/categories/intel/atom.xml
@@ -4,7 +4,7 @@
   <title><![CDATA[Category: Intel | A weblog by Bryan]]></title>
   <link href="http://www.nexus-software.ie/blog/categories/intel/atom.xml" rel="self"/>
   <link href="http://www.nexus-software.ie/"/>
-  <updated>2017-06-07T20:33:48+01:00</updated>
+  <updated>2017-07-06T10:19:07+01:00</updated>
   <id>http://www.nexus-software.ie/</id>
   <author>
     <name><![CDATA[Bryan O'Donoghue]]></name>
@@ -18,7 +18,7 @@
     <link href="http://www.nexus-software.ie/blog/2017/06/05/screaming-fast-gpio-with-intel-galileo/"/>
     <updated>2017-06-05T09:37:45+01:00</updated>
     <id>http://www.nexus-software.ie/blog/2017/06/05/screaming-fast-gpio-with-intel-galileo</id>
-    <content type="html"><![CDATA[<p><img src="images/galileo.jpg"></p>
+    <content type="html"><![CDATA[<p><img src="/images/galileo.jpg"></p>
 
 <h1>Foreword</h1>
 

public/blog/categories/linux/atom.xml
@@ -4,7 +4,7 @@
   <title><![CDATA[Category: Linux | A weblog by Bryan]]></title>
   <link href="http://www.nexus-software.ie/blog/categories/linux/atom.xml" rel="self"/>
   <link href="http://www.nexus-software.ie/"/>
-  <updated>2017-06-07T20:33:48+01:00</updated>
+  <updated>2017-07-06T10:19:07+01:00</updated>
   <id>http://www.nexus-software.ie/</id>
   <author>
     <name><![CDATA[Bryan O'Donoghue]]></name>
@@ -18,7 +18,7 @@
     <link href="http://www.nexus-software.ie/blog/2017/06/05/screaming-fast-gpio-with-intel-galileo/"/>
     <updated>2017-06-05T09:37:45+01:00</updated>
     <id>http://www.nexus-software.ie/blog/2017/06/05/screaming-fast-gpio-with-intel-galileo</id>
-    <content type="html"><![CDATA[<p><img src="images/galileo.jpg"></p>
+    <content type="html"><![CDATA[<p><img src="/images/galileo.jpg"></p>
 
 <h1>Foreword</h1>
 

public/blog/categories/x86/atom.xml
@@ -4,7 +4,7 @@
   <title><![CDATA[Category: X86 | A weblog by Bryan]]></title>
   <link href="http://www.nexus-software.ie/blog/categories/x86/atom.xml" rel="self"/>
   <link href="http://www.nexus-software.ie/"/>
-  <updated>2017-06-07T20:33:48+01:00</updated>
+  <updated>2017-07-06T10:19:07+01:00</updated>
   <id>http://www.nexus-software.ie/</id>
   <author>
     <name><![CDATA[Bryan O'Donoghue]]></name>
@@ -18,7 +18,7 @@
     <link href="http://www.nexus-software.ie/blog/2017/06/05/screaming-fast-gpio-with-intel-galileo/"/>
     <updated>2017-06-05T09:37:45+01:00</updated>
     <id>http://www.nexus-software.ie/blog/2017/06/05/screaming-fast-gpio-with-intel-galileo</id>
-    <content type="html"><![CDATA[<p><img src="images/galileo.jpg"></p>
+    <content type="html"><![CDATA[<p><img src="/images/galileo.jpg"></p>
 
 <h1>Foreword</h1>
 

public/index.html
@@ -88,7 +88,7 @@ which I was the leading Linux engineer. One problem we had &hellip;">
   </header>
 
 
-  <div class="entry-content"><p><img src="images/galileo.jpg"></p>
+  <div class="entry-content"><p><img src="/octopress/public/images/galileo.jpg"></p>
 
 <h1>Foreword</h1>
 

source/_posts/2017-06-05-screaming-fast-gpio-with-intel-galileo.markdown
@@ -5,7 +5,7 @@ date: 2017-06-05 09:37:45 +0100
 comments: true
 categories: [x86, Intel, Galileo, Arduino, Linux]
 ---
-{% img images/galileo.jpg %}
+{% img /images/galileo.jpg %}
 
 Foreword
 ========